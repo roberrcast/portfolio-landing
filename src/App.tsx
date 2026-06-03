a1c2278c399c8ed71f6fafc6c3c218da69dd230b
robercast <dunamis1.16@gmail.com>
2026-06-03 14:31:01 -0700

    Feat: add philosophy component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
+import { Philosophy } from "./components/Philosophy";
 
 function App() {
     return (
         <>
             <Header />
             <Hero />
+            <Philosophy />
         </>
     );
 }
---
+++ src/components/Philosophy/index.tsx
@@ -0,0 +1,17 @@
+import * as S from "./styles";
+
+export const Philosophy = () => {
+    return (
+        <S.PhilosophyContainer>
+            <S.StaveLine />
+
+            <S.QuoteContent>
+                <S.QuoteText>
+                    "Capturando la textura del tiempo a través del lente de la
+                    precisión moderna."
+                </S.QuoteText>
+                <S.Divider />
+            </S.QuoteContent>
+        </S.PhilosophyContainer>
+    );
+};
